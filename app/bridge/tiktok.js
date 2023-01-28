0d0de25df15f8926c06fbb0f1153d31be723e731
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-01-28 02:46:56 -0600

    Added events to bridge

--- app/bridge/tiktok.js
+++ app/bridge/tiktok.js
@@ -23,3 +23,26 @@ exports.disconnect = async function () {
 exports.onChat = function (callback) {
   ipcRenderer.on("tiktok:chat", (_, data) => callback(data));
 };
+
+exports.onCommand = function (callback) {
+  ipcRenderer.on("tiktok:command", (_, data) => callback(data));
+};
+
+exports.onFollower = function (callback) {
+  ipcRenderer.on("tiktok:follower", (_, data) => callback(data));
+};
+
+exports.onShare = function (callback) {
+  ipcRenderer.on("tiktok:share", (_, data) => callback(data));
+};
+
+exports.onGift = function (callback) {
+  ipcRenderer.on("tiktok:gift", (_, data) => callback(data));
+};
+
+exports.onDisconnect = function (callback) {
+  ipcRenderer.on("tiktok:disconnect", (_, data) => callback(data));
+};
+exports.onStreamEnd = function (callback) {
+  ipcRenderer.on("tiktok:end", (_, data) => callback(data));
+};
